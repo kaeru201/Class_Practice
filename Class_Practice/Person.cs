c15e6db3113ec75c140e660dc3111d69d5d3addb
DESKTOP-I3PECJ7\kawakupo <ryomaryoma123456@gmail.com>
2025-08-26 12:52:36 +0900

    Class　練習　初手

---
+++ Class_Practice/Person.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_Practice
+{
+    internal class Person
+    {
+        //変数
+        String name; //キャラの名前
+        int level; //レベル
+        int hp; //体力
+        string info = "ただの村人";// キャラ属性の特徴
+
+        //実体化した際に基本的な情報は入るように設計
+        //引数なしコンストラクタ
+        public Person() 
+        {
+            name = "イッパンジーン";
+            level = 1;
+            hp = 100;
+        }
+        //引数ありコンストラクタ
+        public Person(String n, int h)
+        {
+            name = n;
+            level = 1;
+            hp = h;
+        }
+
+        //命令（メソッド）
+        //そのキャラの情報を表示するメソッド
+        public void InfoDisplay()
+        {
+            Console.WriteLine("----------------------");
+            Console.WriteLine("名前:" + name);
+            Console.WriteLine("レベル:" + level);
+            Console.WriteLine("体力:" + hp);
+            Console.WriteLine(info);
+
+        }
+    }
+}
---
+++ Class_Practice/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_Practice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //自作したPersonクラス(Person型)の実体化(インスタンス)
+            Person p1 = new Person("ムラビート",150);
+            p1.InfoDisplay(); //p1にメソッドを使わせてみる
+        }
+    }
+}
